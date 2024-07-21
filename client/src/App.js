028a3b4966715a59295ccd4f826acb5e13e2a931
MhaskeAditya42 <adityamhaske711@gmail.com>
2024-07-21 17:30:43 +0530

    Added new features

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Graph from "./components/Graph";
 import Form from "./components/Form";
+import Home from "./components/home";
+import Navbar from "./components/Navbar";
+import ExpenseTrack from "./components/ExpenseTrack";
+import GameHome from "./components/GameHome";
+import ChooseModule from "./components/ChooseModule";
+import LeaderBoard from "./components/LeaderBoard";
+import SignIn from "./components/SignIn";
+import Login from "./components/Login";
+import StoryLine from "./components/StoryLine";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
-          {" "}
-          Expense Tracker
-        </h1>
 
-        {/* gird coloums  */}
-        <div className="grid md:grid-cols-2 gap-4">
-          {/* Graph */}
-          <Graph></Graph>
-          {/* Form */}
-          <Form></Form>
-        </div>
-      </div>
+    <Router>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/expenseTrack" element={<ExpenseTrack/>} />
+        <Route path="/storyLine" element={<StoryLine/>} />
+        <Route path="/leaderBoard" element={<LeaderBoard/>} />
+        <Route path="/chooseModule" element={<ChooseModule/>} />
+        <Route path="/gameHome" element={<GameHome/>} />
+        <Route path="/signIn" element={<SignIn/>} />
+        <Route path="/login" element={<Login/>} />
+
+        <Route path="/" element={<Home/>} />
+
+      </Routes>
     </div>
+  </Router>
+    // <>
+    // <Navbar></Navbar>
+    // <div className="App">
+    //   <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        
+    //       <Home></Home>
+        
+    //   </div>
+    // </div>
+   
+    // </>
   );
 }
 
